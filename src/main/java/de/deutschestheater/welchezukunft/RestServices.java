3ffbbc6e5c5f75f04992b505bbb702a04d33c724
lars <l.parmakerli@googlemail.com>
2017-07-21 14:34:56 +0200

    start process with processbuilder

--- src/main/java/de/deutschestheater/welchezukunft/RestServices.java
+++ src/main/java/de/deutschestheater/welchezukunft/RestServices.java
@@ -1,17 +1,18 @@
 package de.deutschestheater.welchezukunft;
 
+import java.io.File;
 import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 @RestController
 public class RestServices {
@@ -20,30 +21,37 @@ public class RestServices {
 	public Principal user(Principal user) {
 		return user;
 	}
-	
+
 	@RequestMapping("/admin/changecontents/")
 	public List<Workshop> createUser(@RequestBody Workshop workshop) {
-        System.out.println("Creating Workshop with LeiterIn " + workshop.getLeiterIn());
-  
-        List<Workshop> entities = new ArrayList<Workshop>();
-       
-        for (int i = 0; i<10; ++i){
-        	entities.add(workshop);
-        }
-        
-        
-        Runtime rt = Runtime.getRuntime();
-        try {
-        	
-            System.out.println("Creating Workshop with LeiterIn " + workshop.getLeiterIn());
-        	
-			Process pr = rt.exec("echo HI | mail -s Test l.parmakerli@googlemail.com");
-		} catch (IOException e) {
-			e.printStackTrace();
+		System.out.println("Creating Workshop with LeiterIn " + workshop.getLeiterIn());
+
+		List<Workshop> entities = new ArrayList<Workshop>();
+
+		for (int i = 0; i < 10; ++i) {
+			entities.add(workshop);
 		}
 
-        
-        return entities;
-    }
-	
+		ProcessBuilder pb = new ProcessBuilder("touch", "testtesttest");
+		Map<String, String> env = pb.environment();
+		env.put("VAR1", "myValue");
+		env.remove("OTHERVAR");
+		env.put("VAR2", env.get("VAR1") + "suffix");
+		pb.directory(new File("/root"));
+		File log = new File("log");
+		pb.redirectErrorStream(true);
+		pb.redirectOutput(Redirect.appendTo(log));
+		Process p;
+		try {
+			p = pb.start();
+			assert pb.redirectInput() == Redirect.PIPE;
+			assert pb.redirectOutput().file() == log;
+			assert p.getInputStream().read() == -1;
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+
+		return entities;
+	}
+
 }
