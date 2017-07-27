873df7eda307a3edac11b280595eb6c6239b06e6
lars <l.parmakerli@googlemail.com>
2017-07-27 19:15:17 +0200

    send test mail to new users

--- src/main/java/de/deutschestheater/welchezukunft/RestServices.java
+++ src/main/java/de/deutschestheater/welchezukunft/RestServices.java
@@ -36,26 +36,6 @@ public class RestServices {
 		workshops.setWorkshop(workshop);	
 		
 
-		/*
-		ProcessBuilder pb = new ProcessBuilder("/bin/sh", "-c", "echo Hallo | mail -s test2 l.parmakerli@googlemail.com");
-		Map<String, String> env = pb.environment();
-		//env.put("VAR1", "myValue");
-		//env.remove("OTHERVAR");
-		//env.put("VAR2", env.get("VAR1") + "suffix");
-		pb.directory(new File("/root"));
-		//File log = new File("log");
-		pb.redirectErrorStream(true);
-		//pb.redirectOutput(Redirect.appendTo(log));
-		Process p;
-		try {
-			p = pb.start();
-			assert pb.redirectInput() == Redirect.PIPE;
-			//assert pb.redirectOutput().file() == log;
-			assert p.getInputStream().read() == -1;
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}*/
-
 		return workshop;
 	}
 	
@@ -86,6 +66,25 @@ public class RestServices {
 	public String addUser(@RequestBody User user){
 		System.out.println("Save new user...");
 		userRepository.save(user);
+		
+		ProcessBuilder pb = new ProcessBuilder("/bin/sh", "-c", "echo Hallo | mail -s test " + user.getMail());
+		Map<String, String> env = pb.environment();
+		//env.put("VAR1", "myValue");
+		//env.remove("OTHERVAR");
+		//env.put("VAR2", env.get("VAR1") + "suffix");
+		pb.directory(new File("/root"));
+		//File log = new File("log");
+		pb.redirectErrorStream(true);
+		//pb.redirectOutput(Redirect.appendTo(log));
+		Process p;
+		try {
+			p = pb.start();
+			assert pb.redirectInput() == Redirect.PIPE;
+			//assert pb.redirectOutput().file() == log;
+			assert p.getInputStream().read() == -1;
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
 		
 		return "success";
 		
