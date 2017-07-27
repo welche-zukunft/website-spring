d8a84383a3d990d79a764f96144950f69d2bcb1e
lars <l.parmakerli@googlemail.com>
2017-07-27 19:14:59 +0200

    enable loggin http arbitrary http requests

--- src/main/java/de/deutschestheater/welchezukunft/Application.java
+++ src/main/java/de/deutschestheater/welchezukunft/Application.java
@@ -3,8 +3,12 @@ package de.deutschestheater.welchezukunft;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.Filter;
+
+import org.apache.catalina.filters.RequestDumperFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +23,14 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
     
-    
 
+    @Bean
+    public FilterRegistrationBean requestDumperFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        Filter requestDumperFilter = new RequestDumperFilter();
+        registration.setFilter(requestDumperFilter);
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
     
 }
