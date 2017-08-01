6e71141442f351746f92327a538ad8833443523a
laparmakerli <l.parmakerli@googlemail.com>
2017-08-01 18:37:29 +0200

    change storage directory

--- src/main/java/de/deutschestheater/welchezukunft/storage/StorageProperties.java
+++ src/main/java/de/deutschestheater/welchezukunft/storage/StorageProperties.java
@@ -8,7 +8,7 @@ public class StorageProperties {
     /**
      * Folder location for storing files
      */
-    private String location = "/home/lars/Documents/uploads";
+    private String location = "/root/uploads";
 
     public String getLocation() {
         return location;
