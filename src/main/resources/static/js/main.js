78176f9c411211af2760b87d5130ac917315ca1e
root <root@welchezukunft.org>
2017-08-01 20:18:34 +0200

    enable ssl

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -50,7 +50,7 @@ angular.module('main', [ 'ngRoute' ])
 
 .factory('ChangeContentService', ['$http', '$q', function($http, $q){
  
-    var REST_SERVICE_URI = 'http://localhost:8080/admin';
+    var REST_SERVICE_URI = 'https://welchezukunft.org/admin';
  
     var factory = {
     	changeWorkshop : changeWorkshop,
