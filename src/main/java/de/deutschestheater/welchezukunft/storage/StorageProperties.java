c8549aa7e483cd10a094e819458dc3d78749e9dc
laparmakerli <l.parmakerli@googlemail.com>
2017-08-03 11:37:17 +0200

    change upload folder

--- src/main/java/de/deutschestheater/welchezukunft/storage/StorageProperties.java
+++ src/main/java/de/deutschestheater/welchezukunft/storage/StorageProperties.java
@@ -8,7 +8,7 @@ public class StorageProperties {
     /**
      * Folder location for storing files
      */
-    private String location = "/root/fileuploads";
+    private String location = "/uploads/zukunft";
 
     public String getLocation() {
         return location;
